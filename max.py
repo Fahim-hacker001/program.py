e6ca966b7b378a21b9e8671ffea10f5abbfc7107
Fahim-hacker001 <92388204+Fahim-hacker001@users.noreply.github.com>
2021-10-12 21:52:13 +0600

    Max.py

---
+++ max.py
@@ -0,0 +1,12 @@
+print("\033[94m")
+import os
+import sys
+import urllib2
+os.system("figlet Web Eye")
+print("\033[91m")
+print("Coded by: Fahim Abrar Niloy ")
+print("\033[96m")
+url = raw_input("Enter Website Link: ")
+url.rstrip ( )
+header = urllib2.urlopen (url) .info ( )
+print(str (header) )
